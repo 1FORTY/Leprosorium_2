481254c1f8f531f57674a2ed85ae59ffc44dc78c
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-09-20 23:02:11 +0700

    Init app

---
+++ app.rb
@@ -0,0 +1,19 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, { adapter: 'sqlite3', database: 'leprosorium.db' }
+
+class Post < ActiveRecord::Base
+
+end
+
+class Comment < ActiveRecord::Base
+
+end
+
+get '/' do
+	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+end
