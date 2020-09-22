8b5b613f443ed8035a7b20cadd33b6dcce5be7a2
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-09-22 22:07:37 +0700

    Add entity index.erb

--- db/migrate/20200920155856_create_comment.rb
+++ db/migrate/20200920155856_create_comment.rb
@@ -1,10 +1,10 @@
 class CreateComment < ActiveRecord::Migration[6.0]
   def change
-  	create_table :comments do |t|
-  		t.text :name
-  		t.text :content
+    create_table :comments do |t|
+      t.text :name
+      t.text :content
 
-  		t.timestamps
-  	end
+      t.timestamps
+    end
   end
 end
