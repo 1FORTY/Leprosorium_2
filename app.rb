4953f995eb70f78ebac2837b4ff9afbd68d959fb
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-09-23 22:54:51 +0700

    Add form for comments

--- app.rb
+++ app.rb
@@ -9,10 +9,11 @@ set :database, { adapter: 'sqlite3', database: 'leprosorium.db' }
 class Post < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3, maximum: 15 }
   validates :content, presence: true, length: { minimum: 10, maximum: 200 }
+  has_many :comments
 end
 
 class Comment < ActiveRecord::Base
-
+  belongs_to :post
 end
 
 before do
@@ -40,5 +41,12 @@ get '/post/:post_id' do
   @post_id = params[:post_id]
   @post = Post.find(@post_id)
 
+  @comments = Comment.where(post_id: @post_id)
+
   erb :post
+end
+
+post '/post/:post_id' do
+  comment = Comment.new params[:com]
+  comment.save
 end
